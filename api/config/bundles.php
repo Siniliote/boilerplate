81a954e1d78391cd3f530494321d1c9645853e03
Stéphane Ferey <stephane.ferey@groupepsih.com>
2021-11-09 14:37:13 +0100

    feat: Ajout de doctrine / nelmio-api-doc-bundle / nelmio-cors-bundle

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -2,4 +2,9 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['dev' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['dev' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
